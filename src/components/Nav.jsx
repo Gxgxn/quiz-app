3fec3842ace62a8bb7033ec5fd7701b45aba8e5f
Gxgxn <gdsinghb99@gmail.com>
2022-11-22 10:41:39 -0400

    added tailwindCss and file structure

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <div className="container mx-auto py-4 flex justify-center items-center  ">
+        <h1 className="text-4xl font-karla">Quiz App</h1>
+      </div>
+    </nav>
+  );
+}
+export default Nav;
